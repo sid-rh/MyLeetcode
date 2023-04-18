6d37eabcfe7a9aa9a8eb7160c28536bfcaa20db9
sid-rh <siddhanth.rh@gmail.com>
2023-04-18 07:27:02 +0530

    FindDisappearedNumbers

---
+++ 448.DisappearedInArray/FindAllNumbersDisappeared.cpp
@@ -0,0 +1,46 @@
+/*
+    Approach: add all the numbers to an unordered map
+    iterate through the range and find the number that does not exist and add it to the answer.
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> findDisappearedNumbers(vector<int> &nums)
+{
+    int n = nums.size();
+    unordered_map<int, int> mp;
+    vector<int> ans;
+    for (auto i : nums)
+    {
+        mp[i]++;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (mp.count(i) == 0)
+            ans.push_back(i);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the numbers: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    vector<int> ans = findDisappearedNumbers(nums);
+
+    for (auto i : ans)
+    {
+        cout << i << " ";
+    }
+}
