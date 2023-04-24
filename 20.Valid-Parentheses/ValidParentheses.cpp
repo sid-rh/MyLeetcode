ec3198910e2094560bdd5dab7efc669f69720973
sid-rh <siddhanth.rh@gmail.com>
2023-04-24 08:11:21 +0530

    NeedleHaystack

--- 20.Valid-Parentheses/ValidParentheses.cpp
+++ 20.Valid-Parentheses/ValidParentheses.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+/*
+ Approach: Use a stack. Iterate through the string and insert any opening parenthesis in it. If closing parenthesis is encountered and the stack is empty return false, if matching closing parenthesis is found, then pop it. If the stack ends up to be empty return true as all parenthesis is matched else return false.
+
+*/
+
 bool isValid(string s)
 {
     stack<char> st;
---
+++ 28.Index-Of-First-Occurence/IndexOfFirstOccurrence.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Approach: find the first occurrence of the first word, find the substring of the size of the needle, if it is equal return index, else continue.
+*/
+
+int strStr(string haystack, string needle)
+{
+
+    for (int i = 0; i < haystack.size(); i++)
+    {
+        if (haystack[i] == needle[0])
+        {
+            string word = haystack.substr(i, needle.size());
+            if (word == needle)
+                return i;
+            else
+                continue;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+    string needle = "";
+    string haystack = "";
+
+    cout << "Enter the haystack: ";
+    cin >> haystack;
+    cout << "Enter the needle: ";
+    cin >> needle;
+
+    bool ans = strStr(haystack, needle);
+
+    cout << ans << endl;
+}
